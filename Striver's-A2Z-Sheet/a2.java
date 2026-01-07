867281dc1a4da4d5ca34ed8d5ae710df660bd7d3
P Bharat Chandra <pbharatchandra80@gmail.com>
2026-01-07 14:13:23 +0530

    uploaded question 2

--- Striver's-A2Z-Sheet/a2.java
+++ Striver's-A2Z-Sheet/a2.java
@@ -1,17 +1,16 @@
-
-public class a1 {
+public class a2 {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+            for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
             }
-            // for (int j = 0; j < i; j++) {
-            // System.out.print(" ");
-            // }
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
             System.out.println();
         }
     }
