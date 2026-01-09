5b688acd1990585f6192795c2962f4bbfc40d82c
P Bharat Chandra <pbharatchandra80@gmail.com>
2026-01-09 08:11:33 +0530

    the diamond/arrow shape

---
+++ Striver's-A2Z-Sheet/a4.java
@@ -0,0 +1,25 @@
+public class a4 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i <= 2 * n - 1; i++) {
+            int stars = i;
+            if (i >= n)
+                stars = 2 * n - i;
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
