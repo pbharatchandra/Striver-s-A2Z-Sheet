5b8f9ab881e65e178f75e9f6be64cd0b4aaa997f
P Bharat Chandra <pbharatchandra80@gmail.com>
2026-01-09 17:25:43 +0530

    ALPHA RAMP PATTERN

--- Striver's-A2Z-Sheet/a9.java
+++ Striver's-A2Z-Sheet/a9.java
@@ -1,19 +1,22 @@
-// Increasing Letter Triangle Pattern
+// Decreasing Letter Triangle Pattern
 
-public class a8 {
+public class a9 {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 0; i < n; i++) {
 
-            for (char j = 'A'; j <= 'A' + i; j++) {
+            for (char j = 'A'; j <= 'A' + (n - i - 1); j++) {
                 System.out.print(j);
             }
             System.out.println();
         }
     }
+
 }
-// A
-// AB
-// ABC
-// ABCD
+
+// (n - i - 1) i.e if n=5 for reverse counting
 // ABCDE
+// ABCD
+// ABC
+// AB
+// A
