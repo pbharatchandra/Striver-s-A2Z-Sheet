7b8b4b152d6a7e82aebb17d31cf4c6c6836ba167
P Bharat Chandra <pbharatchandra80@gmail.com>
2026-01-11 10:35:35 +0530

    HOLLOW RECTANGLE PATTERN

---
+++ Striver's-A2Z-Sheet/a15.java
@@ -0,0 +1,23 @@
+public class a15 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            // STARS
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("-");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
+// HOLLOW RECTANGLE PATTER
+// *****
+// *---*
+// *---*
+// *---*
+// *****
