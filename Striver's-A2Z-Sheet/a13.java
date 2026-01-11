51601af1b16707a0cd4d3169d64b5294b1919f27
P Bharat Chandra <pbharatchandra80@gmail.com>
2026-01-11 10:26:39 +0530

    SYMMETRIC VOID PATTERN

--- Striver's-A2Z-Sheet/a13.java
+++ Striver's-A2Z-Sheet/a13.java
@@ -1,11 +1,54 @@
+//SYMMETRIC VOID PATTERN
 public class a13 {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = 0; i < n; i++) {
-            for (char ch = 'A'; ch <= 'A' + i; ch++) {
-                System.out.print(ch);
+        int n = 8, c = 0, cc = 2 * (n - 1);
+
+        for (int i = 1; i <= n; i++) {
+
+            // STARS
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print("*");
+            }
+            // SPACE
+            for (int j = 1; j <= c; j++) {
+                System.out.print("-");
+
+            }
+            // STARS
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print("*");
             }
             System.out.println();
+            c += 2;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            // STARS
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // // SPACE
+            for (int j = 1; j <= cc; j++) {
+                System.out.print("-");
+            }
+
+            // // STARS
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            cc -= 2;
         }
     }
 }
+
+// **********
+// ****--****
+// ***----***
+// **------**
+// *--------*
+// *--------*
+// **------**
+// ***----***
+// ****--****
+// **********
